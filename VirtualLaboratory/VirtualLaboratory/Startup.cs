6bc23021a0e5192ee9c88230a78abdc47d219678
Re9Wine <asckortan@gmail.com>
2023-01-17 00:22:46 +0300

    Add connection between IRepository and Repository and IService and Service, db contex

--- VirtualLaboratory/VirtualLaboratory/Startup.cs
+++ VirtualLaboratory/VirtualLaboratory/Startup.cs
@@ -2,9 +2,15 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using VirtualLaboratory.DAL;
+using VirtualLaboratory.DAL.Interfaces;
+using VirtualLaboratory.DAL.Repositories;
+using VirtualLaboratory.Service.Implementations;
+using VirtualLaboratory.Service.Interfaces;
 
 namespace VirtualLaboratory
 {
@@ -21,7 +27,54 @@ namespace VirtualLaboratory
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllersWithViews();
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddDbContext<VirtualLaboratoryContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IConstantInLaboratoryWorkRepository, ConstantInLaboratoryWorkRepository>();
+            services.AddScoped<IConstantInLaboratoryWorkService, ConstantInLaboratoryWorkService>();
+
+            services.AddScoped<IConstantRepository, ConstantRepository>();
+            services.AddScoped<IConstantService, ConstantService>();
+
+            services.AddScoped<IEquipmentParametrRepository, EquipmentParametrRepository>();
+            services.AddScoped<IEquipmentParametrService, EquipmentParametrService>();
+
+            services.AddScoped<IEquipmentRepository, EquipmentRepository>();
+            services.AddScoped<IEquipmentService, EquipmentService>();
+
+            services.AddScoped<IInstallationRepository, InstallationRepository>();
+            services.AddScoped<IInstallationService, InstallationService>();
+
+            services.AddScoped<ILaboratoryWorkRepository, LaboratoryWorkRepository>();
+            services.AddScoped<ILaboratoryWorkService, LaboratoryWorkService>();
+
+            services.AddScoped<IMistakeInReportRepository, MistakeInReportRepository>();
+            services.AddScoped<IMistakeInReportService, MistakeInReportService>();
+
+            services.AddScoped<IPhenomenonInLaboratoryWorkRepository, PhenomenonInLaboratoryWorkRepository>();
+            services.AddScoped<IPhenomenonInLaboratoryWorkService, PhenomenonInLaboratoryWorkService>();
+
+            services.AddScoped<IPhenomenonRepository, PhenomenonRepository>();
+            services.AddScoped<IPhenomenonService, PhenomenonService>();
+
+            services.AddScoped<IProcessInPhenomenonRepository, ProcessInPhenomenonRepository>();
+            services.AddScoped<IProcessInPhenomenonService, ProcessInPhenomenonService>();
+
+            services.AddScoped<IProcessRepository, ProcessRepository>();
+            services.AddScoped<IProcessService, ProcessService>();
+
+            services.AddScoped<IReportRepository, ReportRepository>();
+            services.AddScoped<IReportService, ReportService>();
+
+            services.AddScoped<ITheoreticalInformationForLaboratoryWorkRepository, TheoreticalInformationForLaboratoryWorkRepository>();
+            services.AddScoped<ITheoreticalInformationForLaboratoryWorkService, TheoreticalInformationForLaboratoryWorkService>();
+
+            services.AddScoped<ITheoreticalInformationRepository, TheoreticalInformationRepository>();
+            services.AddScoped<ITheoreticalInformationService, TheoreticalInformationService>();
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
